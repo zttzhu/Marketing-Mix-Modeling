bd489e273a85a2f9e94a9308718597a4727a57dd
zttzhu <zttzhu123@gmail.com>
2024-10-09 23:11:17 -0400

    data explore

--- mmm_script.py
+++ mmm_script.py
@@ -2,4 +2,13 @@
 print("Hello Marketing Mix Modeling")
 # %%
 import numpy as np
-import pandas as py
+import pandas as pd
+# %%
+mmm_data = pd.read_csv("data.csv")
+# %%
+mmm_data
+# %%
+mmm_data.info()
+mmm_data.columns
+mmm_data.head()
+# %%
