c19efe04ede699ee4d435eb2c7a35a84f7130287
zttzhu <zttzhu123@gmail.com>
2024-10-14 16:31:40 -0400

    push changes for EDA and data check

--- mmm_script.py
+++ mmm_script.py
@@ -3,6 +3,8 @@ print("Hello Marketing Mix Modeling")
 # %%
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plot
 # %%
 mmm_data = pd.read_csv("data.csv")
 # %%
@@ -11,6 +13,9 @@ mmm_data
 # understand data
 mmm_data.describe()
 mmm_data.columns
+# Data is at weekly level and end at Sunday
+mmm_data['wk_strt_dt'] = pd.to_datetime(mmm_data['wk_strt_dt'])
+mmm_data['wk_strt_dt'].dt.dayofweek.unique()
 # %%
 # 1. Media Variables 
 # media impression columns
@@ -18,9 +23,31 @@ mdip_col = [col for col in mmm_data.columns if "mdip_" in col]
 # media spend columns
 mdsp_col = [col for col in mmm_data.columns if "mdsp_" in col]
 
-# 2. Base Variables (Control variables)
+# 2. Base Variables
 # macro economics variables 
 me_col = [col for col in mmm_data.columns if "me_" in col]
+# Holiday Variables
+hol_col = [col for col in mmm_data.columns if "hldy_" in col]
+# Seasonal Variables
+sea_col = [col for col in mmm_data.columns if "seas_" in col]
+# Store count Variables
+st_col = [col for col in mmm_data.columns if "st_" in col]
+# Discount
+dis_col = [col for col in mmm_data.columns if "mkrdn_" in col]
+base_vars = me_col+hol_col+sea_col+st_col+dis_col
+
+# 3. Sales Variables
+sales_col = ['sales']
+# %% 
+# EDA 
+# Heatmap between impressions and sales - sales has the strongest correlation with mdip_vidtr
+plot.figure(figsize=(10,8))
+sns.heatmap(mmm_data[mdip_col+sales_col].corr(),square=True,annot=True,vmax=1,vmin=-1,cmap="RdBu")
+# Spend and sales: mdsp_vidtr has the strongest correlation with sales
+sns.heatmap(mmm_data[mdsp_col+sales_col].corr(),square=True,annot=True,vmax=1,vmin=-1,cmap="RdBu")
+# Draw distribution
+sns.histplot(mmm_data[sales_col])
+sns.pairplot(mmm_data[mdip_col+sales_col],x_vars= mdip_col,y_vars=sales_col)
 # %%
 # create adstock function
 def adstock(support,half_life):
