bc52e60bf61367e8842363eb42e84f2ea0a89648
zttzhu <zttzhu123@gmail.com>
2025-01-05 23:34:02 -0500

    push changes

---
+++ mmm_project.py
@@ -0,0 +1,13 @@
+# %%
+print("Hello Marketing Mix Modeling")
+# %%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plot
+pd.set_option('display.float_format', '{:.2f}'.format)
+
+# %%
+mmm_data = pd.read_csv("mmm_data.csv")
+# %%
+# 
